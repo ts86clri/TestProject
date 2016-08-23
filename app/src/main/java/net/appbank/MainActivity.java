1e33ebeff5a3bac3710cb6f87440a6bb882715be
佐藤 達也 <tatsuya.sato@ALM0028.local>
2016-08-23 16:18:17 +0900

    Buttonを押したらCheckBoxのチェックが変わるのと EditTextの文字を取得し「Androidくん」の部分が変わるようになりました EditTextに何も入力がない場合「エラー」と文字が表記されます

--- app/src/main/java/net/appbank/MainActivity.java
+++ app/src/main/java/net/appbank/MainActivity.java
@@ -1,7 +1,14 @@
 package net.appbank;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        final TextView textView = (TextView)findViewById(R.id.textview);
+        final EditText editText = (EditText)findViewById(R.id.edittext);
+        final CheckBox checkBox = (CheckBox)findViewById(R.id.checkbox1);
+        button.setText("しゃべる");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBox.isChecked()==false)
+                {
+                    checkBox.setChecked(true);
+                }else{
+                checkBox.setChecked(false);
+                }
+                String text = editText.getText().toString();
+                textView.setText(text);
+                if(text.length()==0)
+                {
+                    textView.setText("エラー");
+                }
+            }
+        });
     }
 }
